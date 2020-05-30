dd09efc3310a2cea3a068cc504b58e1ab4865863
wuyujin <yjwu.bach@outlook.com>
2020-05-30 09:17:29 +0800

    提交配置文件

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 11,
+  },
+  'rules': {
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,18 @@
+console.log(123);
+
+console.error(456);
+
+window.name;
+
+
+console.log('hello 世界');
+
+console.log(1);
+
+/* eslint-disable  */
+
+console.log(2);
+
+/* eslint-enable */
+
+console.log(3);
